9303345f5067a96e243497bee7d0753763048831
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-03-01 13:09:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/ball')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error))
+  }, [])
+  console.log(countries);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Countries loaded: {countries.length}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
